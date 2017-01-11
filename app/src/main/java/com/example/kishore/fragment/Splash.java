9695db234e9dec810a8a46492b6ac24b0aee28a6
kishoreraju2 <kishoreraju.1998@gmail.com>
2017-01-11 23:54:22 +0530

    Added Splash screen. Should create a good looking Image for the app better India

---
+++ app/src/main/java/com/example/kishore/fragment/Splash.java
@@ -0,0 +1,30 @@
+package com.example.kishore.fragment;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.splash);
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(Splash.this,MainActivity.class);
+                Splash.this.startActivity(mainIntent);
+                Splash.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
